6e8f35c07d985651efda41bbe72f72cdb4f5a743
jland972 <jeremy.cledelin@isen.yncrea.fr>
2017-03-05 21:01:09 +0100

    test

--- robot_sonic_asservis/robot_sonic_asservis.ino
+++ robot_sonic_asservis/robot_sonic_asservis.ino
@@ -14,10 +14,19 @@ int codeuse1=4;
 int codeuse2=5;
 unsigned int tick_1=0;
 unsigned int tick_2=0;
-int cmd=0;
+int cmd1=0;
+int cmd2=0;
 const int frequence=50;
-float consigne=10;
-float kp=250; // essayer : 100 ou 300
+float consigne=15;
+float kp1=150;
+float kp2=150;// essayer : 100 ou 300
+float ki1=2;
+float ki2=2;
+float kd1=300;
+float kd2=300;
+float somme_erreur = 0;
+float delta_erreur=0;
+float erreur_precedente=0;
 
 int i;
 int j;
@@ -73,19 +82,22 @@ void asservissement1(){
   int frequence_codeuse=frequence*tick_1;
   float nb_tour_par_sec=frequence_codeuse/20;
   float erreur =consigne-nb_tour_par_sec;
+  float delta_erreur = erreur-erreur_precedente;
+  erreur_precedente = erreur;
+  somme_erreur += erreur;
  
   
-  cmd=kp*erreur;
-  if (cmd<0)cmd=0;
-  else if (cmd>255)cmd=255;
-  analogWrite(motor1_enablePin,cmd);
+  cmd1=kp1*erreur+ki1*somme_erreur+kd1*delta_erreur;
+  if (cmd1<0)cmd1=0;
+  else if (cmd1>255)cmd1=255;
+  analogWrite(motor1_enablePin,cmd1);
   //Serial.println(tick_1);
    // DEBUG
     
     Serial.print(nb_tour_par_sec,8);
-   Serial.print(" : ");
+   //Serial.print(" : ");
    // Serial.print(erreur,4);
-   // Serial.println();
+   Serial.println();
     //*/
     tick_1=0;
 }
@@ -94,19 +106,21 @@ void asservissement2(){
   int frequence_codeuse=frequence*tick_2;
   float nb_tour_par_sec=frequence_codeuse/20;
   float erreur =consigne-nb_tour_par_sec;
- 
+  float delta_erreur = erreur-erreur_precedente;
+  erreur_precedente = erreur;
+  somme_erreur += erreur;
   
-  cmd=kp*erreur;
-  if (cmd<0)cmd=0;
-  else if (cmd>255)cmd=255;
-  analogWrite(motor2_enablePin,cmd);
+  cmd2=kp2*erreur+ki2*somme_erreur+kd2*delta_erreur;
+  if (cmd2<0)cmd2=0;
+  else if (cmd2>255)cmd2=255;
+  analogWrite(motor2_enablePin,cmd2);
   //Serial.println(tick_2);
    // DEBUG
     
     Serial.print(nb_tour_par_sec,8);
    // Serial.print(" : ");
    // Serial.print(erreur,4);
-   // Serial.println();
+    Serial.println();
     //*/
     tick_2=0;
     }
@@ -139,8 +153,8 @@ timer.setInterval(1000/frequence,asservissement2);
 
 void loop() {
   timer.run();
-   d=distanceFunction();
-    mouvement(d);
+  // d=distanceFunction();
+   // mouvement(d);
  
   delay(10);
 }
